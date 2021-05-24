4f1fb80882d10688f6ab27cb8e94bf8ef4893206
Danylov <ol_danylov@ukr.net>
2021-05-24 17:16:57 +0300

    Spawn objects in different locations, collision and trigger event handling.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10.0f;
-    private float zBound = 9.5f;
+    private float speed = 100.0f;
+    private float zBound = 9;
     private Rigidbody playerRb;
     
-    // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -37,5 +36,15 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y,
                 zBound * Math.Sign(transform.position.z));
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) Debug.Log("Player has collided with enemy.");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup")) Destroy(other.gameObject);
     }
 }
